1ed60653c852effc5f16c53b86411b20013bb43c
Robin Beer <r.beer@outlook.de>
2020-03-31 22:46:22 +0200

    quite some progress

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,15 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("blog_list", views.blog_list, name="blog_list"),
-    # path("<int:pk>", views.blog_detail, name="blog_detail"),
-    # path("new", views.blog_new, name="blog_new"),
+    path("", views.blog_list_view, name="blog_list"),
+    path("home", views.home, name="home"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("blog_list", views.blog_list_view, name="blog_list"),
+    path("blog_detail", views.blog_detail, name="blog_detail"),
+    path("blog_new", views.blog_new, name="blog_new"),
+    path("<int:pk>", views.blog_detail, name="blog_detail"),
+    path("new", views.blog_new, name="blog_new"),
     # path("edit/<int:pk>", views.blog_edit, name="blog_edit"),
     # path("delete/<int:pk>", views.blog_delete, name="blog_delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,41 @@
 from datetime import datetime
 
 from django.shortcuts import redirect, render
+from django.views.generic import ListView
 
 from .forms import BlogForm
+from .models import Blog
 
 
-def blog_list(request):
-    return render(request, "blog/blog_list.html")
+def home(request):
+    return render(request, "blog/home.html")
+
+
+def about(request):
+    return render(request, "blog/about.html")
+
+
+def contact(request):
+    return render(request, "blog/contact.html")
+
+
+class BlogListView(ListView):
+    """Renders the home page, with a list of all messages."""
+
+    model = Blog
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogListView, self).get_context_data(**kwargs)
+        return context
+
+
+blog_list_view = BlogListView.as_view(
+    queryset=Blog.objects.order_by("-log_date")[
+        :5
+    ],  # :5 limits the results to the five most recent
+    context_object_name="blog_list",
+    template_name="blog/blog_list.html",
+)
 
 
 def blog_detail(request):
